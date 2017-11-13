338602a52ae18d9683e7e1a7cf535b45fbb43e14
tamkylet <kyle_tam1@hotmail.com>
2017-11-12 19:40:02 -0800

    signin page and sessions module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 class UsersController < ApplicationController
   def new
-  	@user = User.new
+	   @user = User.new
   end
 
   def show
-	@user = User.find(params[:id])
+	   @user = User.find(params[:id])
   end
 
   def create
-	@user = User.new(user_params)
+	   @user = User.new(user_params)
 
-	if @user.save
-	    redirect_to @user
-    else
-      render 'new'
-    end
+  	 if @user.save
+  	     redirect_to @user
+     else
+         render 'new'
+     end
   end
 
   private
 
   # accept only these attributes for security
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   resources :users
 
+  # sessions (signup and signout) named routes
+  get '/login', to: 'sessions/new'
+  post '/login', to: 'sessions/create'
+  delete '/login', to: 'sessions/destroy'
+
   # default view pages named routes
   root 'default_view_pages#home'
   get '/tutorial', to: 'default_view_pages#tutorial'
   get '/about', to: 'default_view_pages#about'
-  get '/contact', to: 'default_view_pages#contact' 
+  get '/contact', to: 'default_view_pages#contact'
 end
