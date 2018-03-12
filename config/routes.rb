92efc3d805cf5e72eac133db12c076fcc9fa6b5b
Kyle Tam <kyle_tam1@hotmail.com>
2018-03-11 19:34:45 -0700

    more ux changes to signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   resources :users
 
-  # sessions (signup and signout) named routes
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
-
   # default view pages named routes
   root 'default_view_pages#home'
   get '/tutorial', to: 'default_view_pages#tutorial'
   get '/about', to: 'default_view_pages#about'
   get '/contact', to: 'default_view_pages#contact'
+
+  # user (sign up and sign in) named routes
+  get '/signup', to: 'users#new'
+
+  # sessions (signup and signout) named routes
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 end
