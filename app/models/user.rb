4657f2eb5712e596905a9b402733665fd3a0243d
Kyle Tam <kyle_tam1@hotmail.com>
2018-03-18 18:19:00 -0700

    ux tweeks + javascript event function

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
                     format: { with: VALID_NAME_REGEX }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 40 },
+  validates :email, presence: true, length: { maximum: 39 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
