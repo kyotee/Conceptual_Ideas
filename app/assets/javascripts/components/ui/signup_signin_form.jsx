40da140d4ddb5298161ad42789a54a156aeb75c6
Kyle Tam <kyle_tam1@hotmail.com>
2018-03-31 20:02:48 -0700

    javascript inital events

--- app/assets/javascripts/components/ui/signup_signin_form.jsx
+++ app/assets/javascripts/components/ui/signup_signin_form.jsx
@@ -6,64 +6,88 @@ window.Post = createReactClass({
 
 
 window.SignupSigninForm = createReactClass({
+	toggleFields(element) {
+
+	},
 	signupEventListener(element, eventsList, action) {
+		this.toggleFields(element);
+
 		var events = eventsList.split(' ');
 
 		for (var counter = 0, eventsList = events.length; counter < eventsList; counter++) {
-			document.getElementById('verify-name').addEventListener(events[counter], action);
+			document.getElementById(element).addEventListener(events[counter], action);
 		}
 	},
-	highlight() {
-		this.signupEventListener('verify-name', 'keypress paste', function() {alert("hello");});
+	componentDidMount() {
+		this.signupEventListener('verify-name-input', 'click keypress paste', function() {
+			this.style.borderColor = "#00aced";
+			document.getElementsByClassName('verify-name')[0].style.color = "#00aced";
+			document.getElementsByClassName('verify-name')[1].style.display = "inline";
+		});
+		this.signupEventListener('verify-email-input', 'click keypress paste', function() {
+			this.style.borderColor = "#00aced";
+			document.getElementsByClassName('verify-email')[0].style.color = "#00aced";
+			document.getElementsByClassName('verify-email')[1].style.display = "inline";
+		});
+		this.signupEventListener('verify-password-input', 'click keypress paste', function() {
+			this.style.borderColor = "#00aced";
+			document.getElementsByClassName('verify-password')[0].style.color = "#00aced";
+			document.getElementsByClassName('verify-password')[1].style.display = "inline";
+		});
+		this.signupEventListener('verify-password-confirm-input', 'click keypress paste', function() {
+			this.style.borderColor = "#00aced";
+			document.getElementsByClassName('verify-password-confirm')[0].style.color = "#00aced";
+			document.getElementsByClassName('verify-password-confirm')[1].style.display = "inline";
+		});
 	},
 
 	render: function () {
 		if(this.props.formType == "Sign up")
-			alert("This is the signup form.");
+			// alert("This is the signup form.");
 
 		return (
 			<div id="signup-table-rails">
 				<table id="signup-table">
 					<tbody>
-					<tr>
-						<th><p>User Name</p></th>
-					</tr>
-					<tr>
-						<td id="verify-name" onChange={this.highlight} >
-							<input type="text" name="name"></input>
-							<p>1 to 12 letters</p>
-						</td>
-					</tr>
-					<tr>
-						<th><p>E-mail</p></th>
-					</tr>
-					<tr>
-						<td id="verify-email">
-							<input type="text" name="email"></input>
-							<p>valid e-mail under 40 characters</p>
-						</td>
-					</tr>
-					<tr>
-						<th><p>Password</p></th>
-					</tr>
-					<tr>
-						<td id="verify-password">
-							<input type="password" name="password"></input>
-							<p>between 6 to 12 characters</p>
-						</td>
-					</tr>
-					<tr>
-						<th><p>Password Confirmation</p></th>
-					</tr>
-					<tr>
-						<td id="verify-password-confirm">
-							<input type="password" name="verify-password"></input>
-							<p>matches password</p>
-						</td>
-					</tr>
-					<tr>
-						<td><button id="accept-button">Submit</button></td>
-					</tr>
+						<tr>
+							<th><p className="verify-name">User Name</p></th>
+						</tr>
+						<tr>
+							<td>
+								<input id="verify-name-input" type="text" name="name"></input>
+								<p className="verify-name">1 to 12 letters</p>
+							</td>
+						</tr>
+						<tr>
+							<th><p className="verify-email">E-mail</p></th>
+						</tr>
+						<tr>
+							<td>
+								<input id="verify-email-input" type="text" name="email"></input>
+								<p className="verify-email">valid e-mail under 40 characters</p>
+							</td>
+						</tr>
+						<tr>
+							<th><p className="verify-password">Password</p></th>
+						</tr>
+						<tr>
+							<td>
+								<input id="verify-password-input" type="password" name="password"></input>
+								<p className="verify-password">between 6 to 12 characters</p>
+							</td>
+						</tr>
+						<tr>
+							<th><p className="verify-password-confirm">Password Confirmation</p></th>
+						</tr>
+						<tr>
+							<td>
+								<input id="verify-password-confirm-input" type="password" name="verify-password"></input>
+								<p className="verify-password-confirm">matches password</p>
+							</td>
+						</tr>
+						<tr>
+							<td><button id="accept-button">Submit</button></td>
+						</tr>
 					</tbody>
 				</table>
 			</div>
