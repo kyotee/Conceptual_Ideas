0ba5c67b4bfea30f93b19d0cc886b4c9c892545b
tamkylet <kyle_tam1@hotmail.com>
2018-05-06 20:30:12 -0700

    more refactoring signup/signin forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-
     if request.xhr?
+      @user = User.new(user_params)
+
       if @user.save
         flash[:alert] = "Green"
         flash[:notice] = "Successfully created an account."
