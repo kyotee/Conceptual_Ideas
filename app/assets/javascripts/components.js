df570345958f9b23aadbb541738ef2667dd118e9
tamkylet <kyle_tam1@hotmail.com>
2018-08-12 03:08:19 -0700

    prep for taking out react_component meth

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -5,3 +5,12 @@ import Root from './components/container/Root.js';
 global.React = React
 global.ReactDOM = ReactDOM
 global.Root = Root
+
+import Counter from './components/ui/Counter';
+
+
+
+ReactDOM.render(
+	<Counter />,
+	document.getElementById('test-123')
+);
--- app/assets/javascripts/components/container/Root.js
+++ app/assets/javascripts/components/container/Root.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom'
-// import { Provider } from 'react-redux';
-// import CounterApp from './CounterApp';
-// import configureStore from '../store/configureStore';
-// import {setCounter} from '../actions/counter';
+import { Provider } from 'react-redux';
+import CounterApp from './CounterApp';
+import configureStore from '../store/configureStore';
+import {setCounter} from '../actions/counter';
 
-// const store = configureStore();
+const store = configureStore();
 
 export default class Root extends Component {
   componentWillMount() {
-    // store.dispatch(setCounter(this.props.counter));
+    store.dispatch(setCounter(this.props.counter));
   }
   render() {
     return (
-        <p>test</p>
-      // <Provider store={store}>
-      //   <CounterApp />
-      // </Provider>
+      <Provider store={store}>
+        <CounterApp />
+      </Provider>
     );
   }
 }
+
