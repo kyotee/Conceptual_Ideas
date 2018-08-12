c84b922649774dbeb1077413481cb92d2078130c
tamkylet <kyle_tam1@hotmail.com>
2018-08-11 18:21:32 -0700

    again

---
+++ app/assets/javascripts/components/state/container/Root.js
@@ -0,0 +1,21 @@
+// import React, { Component } from 'react';
+// import { render } from 'react-dom'
+// import { Provider } from 'react-redux';
+// import UserApplications from './user_applications.jsx.erb';
+// import configureStore from '../store/configureStore';
+// import {setCounter} from '../actions/counter'
+
+// const store = configureStore();
+
+// export default class Root extends Component {
+//   componentWillMount() {
+//     store.dispatch(setCounter(this.props.counter));
+//   }
+//   render() {
+//     return (
+//       <Provider store={store}>
+//         <UserApplications />
+//       </Provider>
+//     );
+//   }
+// }
