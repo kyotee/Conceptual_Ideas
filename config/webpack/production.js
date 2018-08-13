ba5e87afc0df2712d1a22118070ae29b0d28ddcb
tamkylet <kyle_tam1@hotmail.com>
2018-08-13 02:52:26 -0700

    another

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,14 +1,3 @@
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-module.exports = {
-    // configuration
-loaders: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules\/(?!(@angular\/common\/src\/facade\/.+))/,
-            loader: 'babel?presets[]=es2015'
-        }
-    ]
-};
