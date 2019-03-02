80dd6ea70694ec340e3db3e0d832639ab41d467b
tamkylet <kyle_tam1@hotmail.com>
2019-03-01 21:53:16 -0800

    penguin moving gif

--- app/assets/javascripts/components/ui/side_navigation_bar.js
+++ app/assets/javascripts/components/ui/side_navigation_bar.js
@@ -8,6 +8,7 @@ class SideNavigationBar extends Component {
 		});
 		document.getElementById("toggle-snow").addEventListener("click", function(){
 			document.querySelector("#snowy").classList.toggle('snow');
+			document.querySelector("#penguin-gif").classList.toggle('snow');
 		});
 	}
 	loggedin() {
@@ -29,29 +30,32 @@ class SideNavigationBar extends Component {
 	}
 	render() {
 		return (
-			<div id="side-navigate">
-				{this.loggedin()}
-				<button id="dropdown-button">Applications
-					<div id="arrow-icon">↑</div>
-				</button>
-				<div id="dropdown-content">
-					<a href="/signup"><div className="idea-dropdown"></div>&nbsp;&nbsp;&nbsp;&nbsp;Sign up Form</a>
-					<a href="/image_differences"><div className="idea-dropdown"></div>&nbsp;&nbsp;&nbsp;&nbsp;Image Differences</a>
-					<a href="/courses_list"><div className="idea-dropdown"></div>&nbsp;&nbsp;&nbsp;&nbsp;Courses</a>
-					<a href="/chatters"><div className="idea-dropdown"></div>&nbsp;&nbsp;&nbsp;&nbsp;Chatter</a>
+			<div>
+				<div id="side-navigate">
+					{this.loggedin()}
+					<button id="dropdown-button">Applications
+						<div id="arrow-icon">↑</div>
+					</button>
+					<div id="dropdown-content">
+						<a href="/signup"><div className="idea-dropdown"></div>&nbsp;&nbsp;&nbsp;&nbsp;Sign up Form</a>
+						<a href="/image_differences"><div className="idea-dropdown"></div>&nbsp;&nbsp;&nbsp;&nbsp;Image Differences</a>
+						<a href="/courses_list"><div className="idea-dropdown"></div>&nbsp;&nbsp;&nbsp;&nbsp;Courses</a>
+						<a href="/chatters"><div className="idea-dropdown"></div>&nbsp;&nbsp;&nbsp;&nbsp;Chatter</a>
+					</div>
+					<a id="top-line" href="/about"><div className="icon-dropdown drop-about"></div>&nbsp;&nbsp;&nbsp;&nbsp;About</a>
+					<a href="/contact"><div className="icon-dropdown drop-contact"></div>&nbsp;&nbsp;&nbsp;&nbsp;Contact</a>
+					<br/><br/>
+					<a href="https://github.com/tamkylet/Conceptual_Ideas-tamkylet" target="_blank"><div className="github"></div></a>
+					{/* Code reference: https://proto.io/freebies/onoff/ */}
+					<div className="onoffswitch">
+						<input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="myonoffswitch"/>
+						<label id="toggle-snow" className="onoffswitch-label" for="myonoffswitch">
+							<span className="onoffswitch-inner"></span>
+							<span className="onoffswitch-switch"></span>
+						</label>
+					</div>
 				</div>
-				<a id="top-line" href="/about"><div className="icon-dropdown drop-about"></div>&nbsp;&nbsp;&nbsp;&nbsp;About</a>
-				<a href="/contact"><div className="icon-dropdown drop-contact"></div>&nbsp;&nbsp;&nbsp;&nbsp;Contact</a>
-				<br/><br/>
-				<a href="https://github.com/tamkylet/Conceptual_Ideas-tamkylet" target="_blank"><div className="github"></div></a>
-				{/* Code reference: https://proto.io/freebies/onoff/ */}
-				<div className="onoffswitch">
-					<input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="myonoffswitch"/>
-					<label id="toggle-snow" className="onoffswitch-label" for="myonoffswitch">
-						<span className="onoffswitch-inner"></span>
-						<span className="onoffswitch-switch"></span>
-					</label>
-				</div>
+				<div id="penguin-gif"></div>
 			</div>
 		)
 	}
