e2bbc0319e3119a564e62d0d3e7d867c3ff21750
tamkylet <kyle_tam1@hotmail.com>
2019-03-05 01:39:23 -0800

    submit message events with enter key

--- app/assets/javascripts/components/ui/chatter.js
+++ app/assets/javascripts/components/ui/chatter.js
@@ -33,12 +33,34 @@ class Chatter extends Component {
 			});
 		}.bind(this));
 
+		// case where enter key is pressed; needs refactoring
 		document.getElementById('msg-input').addEventListener("keypress", function(e) {
 			var key = e.which || e.keyCode;
 
-			if (e.keyCode == 13)
-				alert("LOL");
-		});
+			if (e.keyCode == 13) {
+				var messageCredentials = {
+					chatter: {
+						  message: document.getElementById('msg-input').value,
+						  username: this.props.username,
+						  user_id: this.props.user_id
+						}
+				};
+
+				document.getElementById('msg-input').value = '';
+
+				$.ajax({
+					type: "POST",
+					url: "/chatters",
+					data: messageCredentials,
+					success: function(data, textStatus, jqXHR) {
+						console.log("Message creation; submission successful.");
+					},
+					error: function(jqXHR, textStatus, errorThrown) {
+						console.log("Message creation; submission unsuccessful.");
+					}
+				});
+			}
+		}.bind(this));
 	}
 	userMessages(messages) {
 		var rows = [];
