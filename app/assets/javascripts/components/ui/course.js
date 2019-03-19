5107038b873ed34c788d58d7dbf5b296d73a2bd9
tamkylet <kyle_tam1@hotmail.com>
2019-03-18 19:38:09 -0700

    course ui work

--- app/assets/javascripts/components/ui/course.js
+++ app/assets/javascripts/components/ui/course.js
@@ -6,23 +6,39 @@ class Course extends Component {
 		super(props);
 	}
 	componentDidMount() {
-		document.getElementsByClassName('tab')[this.props.position].style.backgroundColor = colors[this.props.courseType];
+		var tab = document.getElementsByClassName('tab')[this.props.position];
+		var tabStatus = document.getElementsByClassName('tab-status')[this.props.position];
+		var tabStatusIcon = document.getElementsByClassName('tab-icon-status')[this.props.position];
+		var moreInfo = document.getElementsByClassName('click-me')[this.props.position];
 
-		document.getElementsByClassName('click-me')[this.props.position].addEventListener("click", function() {
+		tab.style.backgroundColor = colors[this.props.courseType];
+
+		tabStatus.addEventListener("click", function() {
+			tabStatus.style.display = "none";
+			tabStatusIcon.style.display = "block";
+		});
+
+		moreInfo.addEventListener("click", function() {
 			document.getElementsByClassName('more-info')[this.props.position].classList.toggle('show');
+			moreInfo.classList.toggle('flip');
 		}.bind(this));
 	}
 	render() {
 		return (
 			<div className="course" id={"course-"+this.props.recordId}>
 				<div className="tab">
-					<p>{this.props.courseid}&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<i>A Boring Course</i></p>
+					<div className="tab-container">
+						<p className="tab-text">{this.props.courseid}&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<i>A Boring Course</i></p>
+						<p className="tab-status">Enroll</p>
+						<div className="tab-icon-status">
+						</div>
+					</div>
 				</div>
 				<div>
-					<p>{this.props.description}</p><br/>
+					<p className="course-desc">{this.props.description}</p><br/>
 				</div>
 				<div>
-					<p className="click-me">^</p>
+					<p className="click-me">↑</p>
 				</div>
 				<div className="more-info">
 					<p>place</p>
