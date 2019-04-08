04a54880bb1a0933a65f7a40905d4796c383b295
tamkylet <kyle_tam1@hotmail.com>
2019-04-08 01:07:25 -0700

    new app link - revamp this* project soon

--- app/assets/javascripts/components/ui/user_applications.js
+++ app/assets/javascripts/components/ui/user_applications.js
@@ -42,6 +42,26 @@ class UserApplications extends Component {
 			);
 		}
 
+		// push new (separate) Node.js (with Express) project
+		boxes.push(
+			<div className="boxes" onClick={() => { window.open('https://conceptual-ideas-node-js.herokuapp.com/', '_blank') }} key={6}>
+				<div className="header-apps">
+					<p>Node.js (with Express) App</p>
+				</div>
+				<div className="image-apps" style={{ backgroundColor: "#CCFFE5" }}>
+					 <div className="idea-icon-apps"></div>​
+				</div>
+				<div className="describe-apps">
+					<p>{apps.app[5].description}</p>
+				</div>
+				<div className="button-apps">
+					<div className="button-apps-container" id={"box-"+6}>
+						<ActionButton text={"See Feature"} />
+					</div>
+				</div>
+			</div>
+		);
+
 		return boxes;
 	}
 	render() {
