ae90bcdc0b37460fd686b865d3a8edb2cc7c909d
tamkylet <kyle_tam1@hotmail.com>
2019-04-30 00:17:38 -0700

    comment for chatter

--- app/assets/javascripts/components/reducers/chatter/chatter.js
+++ app/assets/javascripts/components/reducers/chatter/chatter.js
@@ -7,7 +7,7 @@ const initalState = {
 export default function counter(state = initalState, action) {
   switch (action.type) {
   case C.SET_MESSAGE:
-    return { ...state, message: state.message };
+    return { ...state, message: state.message };   // ability to provide placeholder text 
   case C.UPDATE_MESSAGE:
     return { ...state, message: action.message };
   default:
