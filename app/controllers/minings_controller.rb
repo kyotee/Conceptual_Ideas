113c416e739c3efd11d2ed22970b400ad8a3e332
tamkylet <kyle_tam1@hotmail.com>
2019-05-11 15:43:48 -0700

    Functional R programming code displayed in html

--- app/controllers/minings_controller.rb
+++ app/controllers/minings_controller.rb
@@ -10,10 +10,26 @@ class MiningsController < ApplicationController
   private
 
   def mining_test
-  	sample_size = 10
-  	R.eval "x <- rnorm(#{sample_size})"
-  	R.eval "summary(x)"
-  	R.eval "sd(x)"
-  	R.eval "plot(x)"
+    sample_size = 10
+
+    htmlString = "<html>"
+    htmlString += "<head><title>R Code Test</title></head>"
+    htmlString += "<body>"
+    htmlString += "<p>Running R code...</p>"
+    begin
+      R.eval "x <- rnorm(#{sample_size})"
+      R.eval "sdx <- sd(x)"
+      htmlString += "<p>Succeeded running R code</p>"
+      htmlString += "<pre>x = #{R.x}</pre>"
+      htmlString += "<pre>sd(x) = #{R.sdx}</pre>"
+
+    rescue => e
+      htmlString += "<p>Failed running R code...</p>"
+      htmlString += "<p>#{e.message}</p>"
+    end
+
+    htmlString += "</html>"
+
+    render html: htmlString.html_safe
   end
 end
