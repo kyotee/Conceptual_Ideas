323ef8499d603aaf684a6a515b29bc4662674181
tamkylet <kyle_tam1@hotmail.com>
2019-05-24 17:03:05 -0700

    data mining with R code setup

--- app/controllers/minings_controller.rb
+++ app/controllers/minings_controller.rb
@@ -19,6 +19,22 @@ class MiningsController < ApplicationController
     htmlString = "Running R code...\n\n"
 
     begin
+      R.eval "diseaseData <- read.csv('#{dir}/csv/heart_disease_dataset.csv', header=TRUE, sep=',')"
+      R.eval "diseaseData$num <- factor(diseaseData$num)"
+
+      R.eval "trainingDataSize = ceiling(nrow(diseaseData) * 0.8)"
+      R.eval "set.seed(1)"
+      R.eval "trainingIndex <- sample(seq_len(nrow(diseaseData)), size = trainingDataSize)"
+      R.eval "trainingData <- diseaseData[trainingIndex,]"
+      R.eval "testingData <- diseaseData[-trainingIndex,]"
+
+      R.eval "trainingData[is.na(trainingData$ca), 'ca'] <- median(trainingData$ca, na.rm = TRUE)"
+      R.eval "trainingData[is.na(trainingData$thal), 'thal'] <- median(trainingData$thal, na.rm = TRUE)"
+      R.eval "testingData[is.na(testingData$ca), 'ca'] <- median(testingData$ca, na.rm = TRUE)"
+      R.eval "testingData[is.na(testingData$thal), 'thal'] <- median(testingData$thal, na.rm = TRUE)"
+
+
+      # template code for pdf (above is real code)
       R.eval "mydata <- read.csv('#{dir}/csv/heart_disease_dataset.csv')"
       R.eval "summary(mydata)"
       R.eval "jpeg('#{dir}/csv/rplot.png', width = 350, height = 350)" if !File.exist?("#{dir}/csv/rplot.png")
