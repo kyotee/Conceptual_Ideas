f44b45b9a9be592ec8fa96e6593ac9977e62a8c2
tamkylet <kyle_tam1@hotmail.com>
2019-05-27 17:01:25 -0700

    data mining work

--- app/controllers/minings_controller.rb
+++ app/controllers/minings_controller.rb
@@ -17,6 +17,8 @@ class MiningsController < ApplicationController
     dir = File.dirname(__FILE__).to_s
     sample_size = 10
     htmlString = "<b>Running R code...</b>\n\n"
+    htmlString1 = ""
+    htmlString2 = ""
 
     begin
       R.eval "library(randomForest)"
@@ -27,13 +29,18 @@ class MiningsController < ApplicationController
       R.eval "rows <- nrow(diseaseData)"
       R.eval "columns <- ncol(diseaseData)"
 
-      htmlString += "Dataset for evaluation has #{R.rows} rows and #{R.columns} columns.\n\n"
-      htmlString += "Imputing missing values of attributes with median of associated known values ...\n\n"
+      htmlString += "Dataset for evaluation has #{R.rows} rows and #{R.columns} columns (representing features).\n\n"
+      htmlString += "Each feature is summarized as follows:\n\n"
+
+      R.eval "ds <- summary(diseaseData)"
+      R.eval "capture.output(ds, file = '#{dir}/csv/disease_summary.txt')"
+
+      htmlString1 += "Imputing missing values of features with median of associated known values ...\n\n"
 
       R.eval "diseaseData[is.na(diseaseData$ca), 'ca'] <- median(diseaseData$ca, na.rm = TRUE)"
       R.eval "diseaseData[is.na(diseaseData$thal), 'thal'] <- median(diseaseData$thal, na.rm = TRUE)"
 
-      htmlString += "Splitting dataset into training and testing data ...\n\n"
+      htmlString1 += "Splitting dataset into training and testing data (80% and 20% respectively) ...\n\n"
 
       R.eval "trainingDataSize = ceiling(nrow(diseaseData) * 0.8)"
       R.eval "set.seed(1)"
@@ -41,40 +48,43 @@ class MiningsController < ApplicationController
       R.eval "trainingData <- diseaseData[trainingIndex,]"
       R.eval "testingData <- diseaseData[-trainingIndex,]"
 
+      htmlString1 += "Conducting random forest analysis for classification ...\n\n"
+
       R.eval "rf <- randomForest(num ~., data = trainingData, ntree=100)"
       R.eval "print(rf)"
+      R.eval "capture.output(rf, file = '#{dir}/csv/forest_summary.txt')"
+      R.eval "png('#{dir}/csv/random_forest.png', height=400)"
+      R.eval "plot(rf)"
+      R.eval "text(rf, pretty = 0)"
+      R.eval "dev.off()"
 
-      R.eval "s <- summary(rf)"
-      R.eval "capture.output(s, file = '#{dir}/csv/summary.txt')"      
-      R.eval "pf <- summary(diseaseData)"
-      R.eval "capture.output(pf, file = '#{dir}/csv/summary1.txt')"          
-
-      # R.eval "decisionTree <- rpart(num ~ age + chol + thal + cp + restecg, data = trainingData)"
-      # R.eval "png('#{dir}/csv/decision_tree.png', height=400)" if !File.exist?("#{dir}/csv/decision_tree.png")
-      # R.eval "par(mar=c(5,4,0,1)+.1)"
-      # R.eval "plot(decisionTree)"
-      # R.eval "text(decisionTree, pretty = 0)"
-      # R.eval "decisionTree"
-      # R.eval "dev.off()"
+      htmlString1 += "Summary of random forest results:\n"
+      htmlString2 += "Based on the error rate, random forest has approximately 60% accuracy."
     rescue => e
       htmlString += "Failed running R code...\n\n"
       htmlString += "#{e.message}"
     end
 
-    textSample = IO.read("#{dir}/csv/summary.txt")
-    textSample1 = IO.read("#{dir}/csv/summary1.txt")
+    diseaseSummary = IO.read("#{dir}/csv/disease_summary.txt") if File.exist?("#{dir}/csv/disease_summary.txt")
+    forestSummary = IO.read("#{dir}/csv/forest_summary.txt") if File.exist?("#{dir}/csv/forest_summary.txt")
 
     Prawn::Document.new do
         text htmlString, :inline_format => true
-        text textSample
-        text textSample1
-        # image "#{dir}/csv/decision_tree.png", :position => :center, :scale => 0.70
+        text diseaseSummary
+        text "\n"
+        text htmlString1, :inline_format => true
+        text forestSummary
+        image "#{dir}/csv/random_forest.png", :position => :center, :scale => 0.70    
+        text "\n"
+        text htmlString2
     end.render 
   end
 
   def delete_file
     dir = File.dirname(__FILE__).to_s
 
-    File.delete("#{dir}/csv/decision_tree.png") if File.exist?("#{dir}/csv/decision_tree.png")
+    File.delete("#{dir}/csv/disease_summary.txt") if File.exist?("#{dir}/csv/disease_summary.txt")
+    File.delete("#{dir}/csv/forest_summary.txt") if File.exist?("#{dir}/csv/forest_summary.txt")
+    File.delete("#{dir}/csv/random_forest.png") if File.exist?("#{dir}/csv/random_forest.png.png")
   end
 end
