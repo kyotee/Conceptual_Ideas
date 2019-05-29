76d3b3272f7977ac2b9570124620cdbb47c0a0f2
tamkylet <kyle_tam1@hotmail.com>
2019-05-29 16:02:27 -0700

    bug fix by including R packages for production

--- init.R
+++ init.R
@@ -1,5 +1,5 @@
 ## Install R packages
-my_packages <- c("randomForest","pROC")
+my_packages <- c("randomForest","pROC","caret","e1071")
 install_if_missing <- function(p) {
 	if(p %in% rownames(installed.packages())==FALSE) {
 		install.packages(p)
